279bf310e9eb5650f12fef0b5e003102cc3faa6a
admin <admin@DESKTOP-I6HUGBR>
2024-12-05 14:30:44 +0900

    First commit in dev branch

--- MyGitTest/src/mygit/MyClass.java
+++ MyGitTest/src/mygit/MyClass.java
@@ -3,5 +3,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("This is the 2nd version!");
+		YourClass your = new YourClass();
+		your.sayHello("Everyone");
 		}
 }
